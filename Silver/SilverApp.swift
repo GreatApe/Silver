a15482fb732517ba2fe98814b6c83651a96323c0
Gustaf Kugelberg <kugel@kth.se>
2025-03-09 19:43:17 +0100

    Initial Commit

---
+++ Silver/SilverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverApp.swift
+//  Silver
+//
+//  Created by Gustaf Kugelberg on 09/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SilverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
